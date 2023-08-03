7a37d61305c0db9e984935a47340bdf528282125
ryanfritsch-storytell <126715608+ryanfritsch-storytell@users.noreply.github.com>
2023-08-03 14:53:49 -0700

    BLOGS

--- sparkle/frontend/src/components/NavHeader/NavHeader.tsx
+++ sparkle/frontend/src/components/NavHeader/NavHeader.tsx
@@ -6,7 +6,11 @@ export default function NavHeader() {
     return (
         <div className="flex flex-row items-center content-center Nav-header">
             <div className="Nav-logo">
-                <img src={logo} alt="Logo" />
+                {/* <img src={logo} alt="Logo" /> */}
+                <img
+                    src="https://p144.p3.n0.cdn.getcloudapp.com/items/rRuzDR8w/643e046b-5017-4beb-8a68-37f2c155099f.jpg?v=a67e850bed952d671b4995463f62d90a"
+                    width="350px"
+                />
             </div>
         </div>
     )
